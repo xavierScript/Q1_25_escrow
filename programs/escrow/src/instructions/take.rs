8f131e4c8beee5ce2b2a053b94e89e35ba78bb3e
xavierScript <david111francisco@gmail.com>
2025-01-29 14:30:16 +0100

    added take.rs

---
+++ programs/escrow/src/instructions/take.rs
@@ -0,0 +1,120 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{associated_token::AssociatedToken, token_interface::{close_account, transfer_checked, CloseAccount, Mint, TokenAccount, TokenInterface, TransferChecked}};
+
+use crate::program::Escrow;
+
+// create context
+#[derive(Accounts)]
+pub struct Take<'info> {
+    #[account(mut)]
+    pub taker: Signer<'info>,
+    #[account(mut)]
+    pub maker: SystemAccount<'info>,
+    pub mint_a: InterfaceAccount<'info, Mint>,
+    pub mint_b: InterfaceAccount<'info, Mint>,
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = mint_a,
+        associated_token::authority = taker,
+    )]
+    pub taker_ata_a: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        mut,
+        associated_token::mint = mint_b,
+        associated_token::authority = taker,
+    )]
+    pub taker_ata_b: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = mint_b,
+        associated_token::authority = maker,
+    )]
+    pub maker_ata_b: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        mut,
+        close = maker,
+        has_one = maker,
+        has_one = mint_a,
+        has_one = mint_b,
+        seeds = [b"escrow", maker.key().as_ref(), escrow.seed.to_le_bytes().as_ref()],
+        bump = escrow.bump,
+    )]
+    pub escrow: Account<'info, Escrow>,
+        // vault
+    #[account(
+    mut,
+    associated_token::mint = mint_a,
+    associated_token::authority = escrow,
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+
+   
+}
+
+
+// Deposit tokens from taker to maker
+// Transfer tokens from vault to taker
+// close vault account
+
+impl<'info> Take<'info> {
+    pub fn deposit(&mut self) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = TransferChecked {
+            from: self.taker_ata_b.to_account_info(),
+            to: self.maker_ata_b.to_account_info(),
+            authority: self.taker.to_account_info(),
+            mint: self.mint_b.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer_checked(cpi_ctx, self.escrow.receive, self.mint_b.decimals)?;
+                Ok(())
+
+    }
+
+    pub fn withdraw_and_close_vault(&mut self) -> Result<()> {
+          let signer_seeds: [&[&[u8]]; 1] = [&[
+            b"escrow",
+            self.maker.key.as_ref(),
+            &self.escrow.seed.to_le_bytes()[..],
+            &[self.escrow.bump]
+        ]];
+
+         // WITHDRAW TOKENS
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.taker_ata_a.to_account_info(),
+            authority: self.escrow.to_account_info(),
+            mint: self.mint_a.to_account_info(),
+        };
+
+        // we use signer seeds because the vault is a pda
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer_seeds);
+
+        transfer_checked(cpi_ctx, self.vault.amount, self.mint_a.decimals)?;
+
+        // CLOSE VAULT ACCOUNT
+        let cpi_program = self.token_program.to_account_info();
+
+        let cpi_accounts = CloseAccount {
+            account: self.vault.to_account_info(),
+            destination: self.maker.to_account_info(),
+            authority: self.escrow.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer_seeds);
+
+        close_account(cpi_ctx)?;
+
+        Ok(())
+    }
+}
--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -1,16 +1,29 @@
 use anchor_lang::prelude::*;
 
+mod state;
+mod instructions;
+
+use instructions::*;
+
 declare_id!("8y7fVVXx9tKtJ1geEJA4CMUQBQohUqnmuHD9oRa3kDDH");
 
 #[program]
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)?;
+        Ok(())
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()?;
+        Ok(())
+    }
+pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
